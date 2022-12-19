21511594f11a34536804d5470f90ee9943c7eb9e
Marc Cataford <mcat@riseup.net>
2022-12-19 15:21:00 -0500

    docs: documented gaps

--- src/base.py
+++ src/base.py
@@ -27,6 +27,12 @@ class Response(typing.TypedDict):
 
 
 class HealthcheckEntry(typing.TypedDict):
+    """
+    Represents a single check reported by the service. A request
+    pushing healthcheck statuses can push as many checks as it needs
+    to report on different metrics.
+    """
+
     name: str
     success: bool
     metric: float
@@ -43,7 +49,46 @@ class HealthcheckRecord(typing.TypedDict):
 
 
 def handler(event, *args, **kwargs) -> Response:
-    http_method = event["requestContext"]["httpMethod"]
+    """
+    Entrypoint to the API.
+
+    A GET will return a collection of all available healthcheck records
+    for all services that reported in (up to the TTL, 24 hours). The
+    returned map of checks is keyed by service name and the structure of the
+    payload is documented in HealthcheckRecord.
+
+    ```
+    GET /
+
+    200 OK {
+        "services": {
+            {service-name}: {HealthcheckRecord for service-name}
+            ...
+        }
+    }
+    ```
+
+    A POST will submit a healthcheck entry to the tracker. If valid, this results
+    in a row being added to the DynamoDB table that tracks healthchecks and returns
+    a 201.
+
+    ```
+    POST /
+
+    201 CREATED {}
+    ```
+
+    Any invalid payload (i.e. non-json request body for POST requests or HTTP verbs
+    other than GET/POST) will result in a 400.
+    """
+
+    http_method: str = event["requestContext"]["httpMethod"]
+
+    # Creating a healthcheck record
+    #
+    # This flow requires a valid json payload that describes the
+    # healthcheck entry (see HealthcheckRecord for structure) and
+    # writes a new entry with TTL (24h) to DynamoDB.
 
     if http_method == "POST":
         try:
@@ -71,6 +116,14 @@ def handler(event, *args, **kwargs) -> Response:
 
         return {"statusCode": 201}
 
+    # Retrieving healthcheck records
+    #
+    # This flow scans the DynamoDB table and returns a collection of
+    # healthcheck records keyed by service-name representing all available
+    # data. Because data is TTL'ed at 24 hours, anything beyond that point
+    # should be disregarded since it should have been marked for deletion
+    # in AWS.
+
     if http_method == "GET":
         entries = ddb_table.scan()
 
@@ -90,6 +143,11 @@ def handler(event, *args, **kwargs) -> Response:
             )
 
         return {"statusCode": 200, "body": json.dumps(checks_map)}
+
+    # Catch-all for any invalid requests
+    #
+    # Any request using a verb other than GET/POST is ignored and results in
+    # a 400.
 
     logger.exception(
         "Handler does not accept HTTP method %s", event["requestContext"]["httpMethod"]
